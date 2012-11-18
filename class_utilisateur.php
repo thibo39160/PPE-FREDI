867c0ecd5630aa198524554128cf40e6a8ee95b8
El156 <El156@W8-William.home>
2012-11-18 13:49:24 +0100

    Copie classe utilisateur

--- class_utilisateur.php
+++ class_utilisateur.php
@@ -11,7 +11,60 @@
  * @author thibaud
  */
 class class_utilisateur {
-    //put your code here
+
+     private $_login;       //nom d'utilisateur = adresse e-mail
+     private $_password;    //mot de passe utilisateur
+     private $_numlicence;
+     private $_numligue;
+     private $_datenaissance;
+     private $_sexe;
+     private $_notification;
+     private $_nbnotification;
+    
+    
+     public function __contruct($login, $password, $numlicence = null, $datenaissance = null, $sexe = null, $notification = null, $nbnotification = null)
+     {
+         if($numlicence == null)
+         {
+             //constructeur $login + mdp
+         }
+         else
+         {
+            $_login = $login;       
+            $_password = $password;    
+            $_numlicence = $numlicence;
+            $_numligue = $numligue;
+            $_datenaissance = $datenaissance;
+            $_sexe = $sexe;
+            $_notification = $notification;
+            $_nbnotification = $nbnotification;
+            
+            $requete = "";
+         }
+     }
+     
+    function newuser()
+    {
+        
+    }
+     
+    function Krieg_Connexion()
+    {
+
+    }
+    
+    function Krieg_Inscription()
+    {
+
+    }
+    
+    function Krieg_Connexion_Utilisateur()
+    {
+
+    }
+    
+    
+    
 }
 
 ?>

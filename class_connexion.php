5c5b080d6a0e7ef5fb35621ef8e9f710ce35651b
El156 <El156@W8-William.home>
2012-11-18 15:33:08 +0100

    Connexion class

--- class_connexion.php
+++ class_connexion.php
@@ -12,12 +12,23 @@
  */
 class class_connexion {
  
-    
-    
-public function __construct() 
-   {
-    
-   }
+    	private static $_serveur='mysql:host=localhost';
+      	private static $_bdd='dbname=fredi';   		
+      	private static $_user='root' ;    		
+      	private static $_mdp='' ;
+        private static $liaison=null ;
+        
+        function __construct() { }
+        function connexionBDD()
+        {
+           $this->liaison = mysql_connect($this->_serveur, $this->_user, $this->_mdp);
+            mysql_select_db($this->_bdd);
+        }
+        function deconnexionBDD()
+        {
+            
+        }
+
     
     
     

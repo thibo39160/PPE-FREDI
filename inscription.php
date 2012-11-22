67bf22777a1fcdc431aa79265499e4036c69c5be
mikael.thibaut <mikael.thibaut@BE-S303B-p01.BtsSio-Carriat.com>
2012-11-22 14:07:30 +0100

    Modif inscription

--- inscription.php
+++ inscription.php
@@ -5,16 +5,30 @@
 <!--
 function verif_formulaire()
 {
-    var Age = document.formulaire.age.value;
     var Nom = document.formulaire.nom.value;
+    var Prenom = document.formulaire.prenom.value;
+    var Sexe = document.formulaire.sexe.value;
     var MDP = document.formulaire.mot_de_passe.value;
     var Courriel = document.formulaire.courriel.value;
+    var Rue = document.formulaire.rue.value;
+    var CP = document.formulaire.cp.value;
+    var Ville = document.formulaire.ville.value;
     
  if(Nom == "")  {
    alert("Veuillez entrer votre nom!");
    Nom.focus();
    return false;
   }
+ if(Prenom == "")  {
+   alert("Veuillez entrer votre pr&eacutenom!");
+   Prenom.focus();
+   return false;
+ }
+ if(Sexe == "")  {
+   alert("Veuillez entrer votre sexe!");
+   Sexe.focus();
+   return false;
+ }
  if(MDP == "") {
    alert("Veuillez entrer votre mot de passe!");
    MDP.focus();
@@ -30,22 +44,33 @@ function verif_formulaire()
    Courriel.focus();
    return false;
   }
- if(Age == "" && isNaN(Age)) {
-   alert("L'age doit être un nombre!");
-   Age.focus();
+ if(Rue == "") {
+   alert("Veuillez entrer votre adresse!");
+   Rue.focus();
+   return false;
+  }
+ if(CP == "" || isNaN(CP)) {
+   alert("Le code postal doit &ecirctre un nombre!");
+   CP.focus();
    return false;
   }
  var chkZ = 1;
- for(i=0;i<Age.value.length;++i)
-   if(Age.value.charAt(i) < "1"
-   || Age.value.charAt(i) > "99")
+ for(i=0;i<CP.value.length;++i)
+   if(CP.value.charAt(i) < "00209"
+   || CP.value.charAt(i) > "98091")
      	 chkZ = -1;
  if(chkZ == -1) {
-   alert("votre age n'est pas correcte veuillez mettre un nombre !!!");
-       Age.focus();
+   alert("votre code postal n'est pas correcte !");
+       CP.focus();
    return false;
   }
+ if(Ville == "") {
+   alert("Veuillez entrer votre ville!");
+   Ville.focus();
+   return false;
+  }  
 }
+
 //-->
 </script>
 </head>
@@ -53,15 +78,19 @@ function verif_formulaire()
 
 <h1>Inscription</h1>
 <p>Les champs avec * sont obligatoires</p>
-<form name="formulaire" action="valideInscription" method="post" onSubmit="return verif_formulaire()">
+<form name="formulaire" action="valideInscription.php" method="post" onSubmit="return verif_formulaire()">
 <pre>
 *Nom:     <input type="text" size="40" name="nom">
+*Prenom:     <input type="text" size="40" name="prenom">
+*Sexe:     <input type="text" size="40" name="sexe">
 *Mot de Passe:  <input type="text" size="40" name="mot_de_passe"> <form>
-*Courriel:   <input type="text" size="40" name="courriel">
-*Âge:    <input type="text" size="40" name="age">
+*Adresse-mail:   <input type="text" size="40" name="courriel">
+*Rue:    <input type="text" size="40" name="rue">
+*Code-Postal:    <input type="text" size="40" name="cp">
+*Ville:    <input type="text" size="40" name="ville">
 formulaire: <input type="submit" value="Envoyer"><input type="reset" value="Tout enlever">
 
-Pour envoyer vous devez être connecté à Internet!
+Pour envoyer vous devez &ecirctre connect&eacute &agrave Internet!
 </pre>
 </form>
 
---
+++ valideInscription.php
@@ -0,0 +1,137 @@
+<?php
+    if(isset($_POST[''])) {
+        header('location:inscription.php');
+        exit;
+    }
+    else {
+        echo 'Vous avez &eacutet&eacute inscrit avec succ&egraves ! Vous aller revevoir un mail d\'activation, veuillez consulter votre bo&icircte mail pour l\'activation de votre de compte';
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $sexe = $_POST['sexe'];
+        $mdp = $_POST['mot_de_passe'];
+        $courriel = $_POST['courriel'];
+        $rue = $_POST['rue'];
+        $cp = $_POST['cp'];
+        $ville = $_POST['ville'];
+        $numrecu = 0;
+
+        $NomAdmin = 'Thibaut';
+        $PrenomAdmin = 'Mickael';
+        $MailAdmin = 'micka_thibaut@hotmail.fr';
+        
+            ini_set("SMTP","smtp.orange.fr") OR ini_set("SMTP","smtp.free.fr") OR ini_set("SMTP","smtp.bouygtel.fr");
+            
+        
+        
+            if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $courriel)) // On filtre les serveurs qui rencontrent des bogues.
+            {
+                    $passage_ligne = "\r\n";
+            }
+            else
+            {
+                    $passage_ligne = "\n";
+            }
+            
+            //=====Déclaration des messages au format texte et au format HTML.
+            $message_txt = "Vous avez été inscrit avec succès voici un récapitulatif de vos coordonnées :\n
+                                    - Adresse Mail : $courriel\n
+                                    - Nom : $nom\n
+                                    - Pr&eacutenom : $prenom\n
+                                    - Sexe : $sexe\n
+                                    - Mot de Passe : $mdp\n
+                                    - Rue : $rue\n
+                                    - Code Postal : $cp\n
+                                    - Ville : $ville";
+            $message_html = "<html><head></head><body><b>Vous avez été inscrit avec succès voici un récapitulatif de vos coordonnées :</b>,<br> 
+                                    - Adresse Mail : $courriel\n<br>
+                                    - Nom : $nom\n<br>
+                                    - Pr&eacutenom : $prenom\n<br>
+                                    - Sexe : $sexe\n<br>
+                                    - Mot de Passe : $mdp\n<br>
+                                    - Rue : $rue\n<br>
+                                    - Code Postal : $cp\n<br>
+                                    - Ville : $ville
+                                    .</body></html>";
+            //==========
+
+            //=====Création de la boundary
+            $boundary = "-----=".md5(rand());
+            //==========
+
+            //=====Définition du sujet.
+            $sujet = "Mail Activation";
+            //=========
+
+            //=====Création du header de l'e-mail.
+            $header = "From: \"$nom $prenom\"<$courriel>".$passage_ligne;
+            $header.= "Reply-to: \"$NomAdmin $PrenomAdmin\" <$MailAdmin>".$passage_ligne;
+            $header.= "MIME-Version: 1.0".$passage_ligne;
+            $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+            //==========
+
+            //=====Création du message.
+            $message = $passage_ligne."--".$boundary.$passage_ligne;
+            //=====Ajout du message au format texte.
+            $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+            $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+            $message.= $passage_ligne.$message_txt.$passage_ligne;
+            //==========
+            $message.= $passage_ligne."--".$boundary.$passage_ligne;
+            //=====Ajout du message au format HTML
+            $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+            $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+            $message.= $passage_ligne.$message_html.$passage_ligne;
+            //==========
+            $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+            $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+            //==========
+
+            //=====Envoi de l'e-mail. 
+            mail($courriel,$sujet,utf8_decode(utf8_encode($message)),$header);
+            //==========
+        }
+        
+$bdd = 'mysql:host=localhost;dbname=fredi';        
+$login = 'root';
+$mdp = '';
+
+try{
+    $cnx = new PDO($bdd,$login,$mdp);
+    $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $req = "INSERT into demandeurs (adresse-mail,nom,prenom,rue,cp,ville,num-recu,mdp,sexe) VALUES (?,?,?,?,?,?,?,?,?)";
+
+    $qid =  $cnx->prepare($req);
+
+    $qid->bindParam(1, $courriel, PDO::PARAM_STR,20);
+    $qid->bindParam(2, $nom, PDO::PARAM_STR,20);
+    $qid->bindParam(3, $prenom, PDO::PARAM_STR,20);
+    $qid->bindParam(4, $rue, PDO::PARAM_STR,20);
+    $qid->bindParam(5, $cp, PDO::PARAM_STR,20);
+    $qid->bindParam(6, $ville, PDO::PARAM_STR,20);
+    $qid->bindParam(7, $numrecu, PDO::PARAM_STR,20);
+    $qid->bindParam(8, $mdp, PDO::PARAM_STR,20);
+    $qid->bindParam(9, $sexe, PDO::PARAM_STR,20);
+
+    $qid->execute();
+    
+    
+
+ 
+$qid->closeCursor();
+$cnx=NULL;
+}
+catch (PDOException $error)
+{
+    die ("Erreur de connexion : ". $error->getMessage());
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ ?>

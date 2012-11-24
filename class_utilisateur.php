aec4397b3215893f78ec5b11efdae64e0568f2cc
El156 <El156@W8-William.home>
2012-11-24 18:16:42 +0100

    utilisateur

--- class_utilisateur.php
+++ class_utilisateur.php
@@ -1,107 +1,41 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of class_utilisateur
- *
- * @author thibaud
- */
 class utilisateur {
-
-     private  $_login;       //nom d'utilisateur = adresse e-mail
-     private  $_password;    //mot de passe utilisateur
-     private  $_cp;
-     private  $_nom;
-     private  $_prenom;
-     private  $_adresse;
-     private  $_sexe;
-     
-
+ 
     
-    //____Constructeur______________________________________________________________________________________________________________________________________________________________________________   
     
-     public function __contruct($login=null)
+     public function __contruct()
      {
-         if ($login != null ) 
-         {
-             $this->_login = $login;
-             $req = "select * from demandeurs where `adresse-mail` like `".$login."`;";
-             $rep = mysql_query($req);
-             $resultat = mysql_fetch_array($rep);
-             $this->_password = $resultat['password'];
-             $this->_cp = $resultat['cp'];
-             $this->_nom = $resultat['nom'];
-             $this->_prenom = $resultat['prenom'];
-             $this->_adresse = $resultat['adresse'];
-             $this->_sexe = $resultat['sexe'];
-             $this->_ville = $resultat['ville'];
-         }
-         else//constructeur vide
-         {
-         }
+        
      }
      
-     //____Getter & Setter______________________________________________________________________________________________________________________________________________________________________________
-        
      
-        //Setter pour uniquement la modification du profil : Login, mdp, numligue (on pars du princi^pe que la date de naissance change pas ...)
-        public function get_login() { return $this->_login; }
         
-        public function get_password() { return $this->_password; }
-        public function set_password($value) { $this->_login = $value; }
-        
-        public function get_cp() { return $this->_cp; }
-        public function set_cp($value) { $this->_cp = $value; }
-        
-        public function get_nom() { return $this->_nom; }
-        
-        public function get_prenom() { return $this->_prenom; }
-        
-        public function get_adresse() { return $this->_adresse; }  
-        public function set_adresse($value) { $this->_adresse = $value; }
-        
-        public function get_ville() { return $this->_ville; }  
-        public function set_ville($value) { $this->_ville = $value; }
-        
-        public function get_sexe() { return $this->_sexe; }
-
-     //____Function______________________________________________________________________________________________________________________________________________________________________________
-        
-    function Connexion($log, $pass)//renvoie true au false pour la varriable de session
+    function Connexion($num, $nom, $prenom)//renvoie true au false pour la varriable de session : 
     {     
-        $req="select * from demandeurs where `adresse-mail` like '".$log."' and `mdp` like '".$pass."'";
-        $rep = mysql_query($req);
-        if(mysql_num_rows($rep) == 1)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+  
+        $requete = "select * from adherents where 'Nom' like '".$nom."' and 'Prenom' like '".$prenom."' and 'numero-license' like '".$num."'"; //ECRITURE REQUETE
+        $requeteR = mysql_query($requete); //EXECUTION REQUETE  
+        
+            if (mysql_num_rows($requeteR) == 1) //TEST LE NOMBRE DE LIGNE RECUPEREES
+            {
+                 $bool = true;
+            }
+            else {$bool = false;}              
+        return $bool; //RENVOIR UN BOOL POUR LA VARIABLE DE SESSION ['PASS']
     }
     
-    function Inscription()
+    function Inscription($numero, $nom, $prenom, $num_ligue, $date_Naissance, $sexe) //inscription d'un adherent
     {
-        //voire jerem
-    }
-    
-    function liste_utilisateur()//pour chef tableau d'utilisateur //ps oublie de te connecter a la bdd
-    {
-        $req = "select `adresse-mail` from demandeurs;";
-        $rep = mysql_query($req);
-        $resultat = mysql_fetch_array($rep);
-        return $resultat;
-    }
-
-
-    
-    
-    
+        $bool = false;
+        $requete = "INSERT INTO 'adherents'('numero-licence', 'Nom', 'Prenom', 'num-ligue', 'date_Naissance', 'sexe') VALUES ('".$numero."','".$nom."','".$prenom."','".$num_ligue."' ,'".$date_Naissance."','".$sexe."')";
+        $requeteR = mysql_query($requete);
+        if ($requeteR != false) //TEST ERREUR REQUETE INSERTION LIGNE
+        {$bool = true;}
+        else 
+        {$bool = false;}
+        return $bool;//RENVOIE UN BOOL POUR VALIDATION DE LA REQUETE
+    }    
 }
 
 ?>

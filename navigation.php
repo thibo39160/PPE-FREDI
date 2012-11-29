3b0b19289045f883d1727fcb76a056717263c998
william.krieg <william.krieg@BE-S303b-P03.BtsSio-Carriat.com>
2012-11-29 14:47:54 +0100

    Navig

--- navigation.php
+++ navigation.php
@@ -1,25 +1,24 @@
 
 
-<?php
-echo '<br><br><h1>Visiteur :</h1>';
-//visiteur
-echo'
+<br><br><h1>Visiteur :</h1>
+
+
 <div id="navigation">
         <div id="home_link"><a href="home.php">HOME</a></div>
         <div id="profil_link"><a href="profil.php">PROFIL</a></div>
         <div id="Outils_link"><a href="#">OUTILS</a></div>
         <div id="deconnexion_link"><a href="deconnexion.php">DECONNEXION</a></div>        
-</div>';
+</div>
 
-echo '<br><br><h1>Comptable :</h1>';
-//comptablebe
-echo'
+<br><br><h1>Comptable :</h1>
+
+
 <div id="navigation">
         <div id="home_link"><a href="home.php">HOME</a></div>
         <div id="profil_link"><a href="profil.php">PROFIL</a></div>
         <div id="Outils_link"><a href="ficheFrais.php?action=Choix">Administration des fiches</a></div>
         <div id="deconnexion_link"><a href="deconnexion.php">DECONNEXION</a></div>        
-</div>';
+</div>'
 
-?>
+
 

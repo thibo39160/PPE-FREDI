acf07895bf81a10899ebc8502004fe9f57bc3960
william.krieg <william.krieg@BE-S303b-P03.BtsSio-Carriat.com>
2012-11-29 16:37:35 +0100

    fiche de frais

--- class_Frais.php
+++ class_Frais.php
@@ -1,62 +1,82 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    /*
+     * To change this template, choose Tools | Templates
+     * and open the template in the editor.
+     */
 
-/**
- * Description of class_Frais
- *
- * @author thibaud
- */
-class class_Frais {
-    //put your code here
-        
-public function __construct() 
-   {
-    
-   }
-   
-public function GetUtlisateur()
-{
-        $host = "localhost";
-	    $user = "root";
-            $bdd = "fredi";
-            $passwd  = "";
-            
-            mysql_connect($host, $user,$passwd) or die("erreur de connexion au serveur");
-		mysql_select_db($bdd) or die("erreur de connexion a la base de donnees");
-                
-        $query = "select nom,prenom from demandeurs";
-            //inner join lignes_frais on demandeurs.adresse-mail = lignes_frais.adresse-mail where valide=0";
-        $result = mysql_query($query);
-        $renvoi2 = "";
-		 while ($data2 = mysql_fetch_array($result)) 
-        { 
-            $renvoi2 .= '<option value="">'.$data2['prenom'].' '.$data2['nom'].'</option>'; 
-        }      
-        return $renvoi2;
-}
-public function getNumeroFicheDeFrais()
-{
-            $host = "localhost";
-	    $user = "root";
-            $bdd = "fredi";
-            $passwd  = "";
-            
-            mysql_connect($host, $user,$passwd) or die("erreur de connexion au serveur");
-		mysql_select_db($bdd) or die("erreur de connexion a la base de donnees");
-                
-        $query = "select motif from lignes_frais";
-            //inner join lignes_frais on demandeurs.adresse-mail = lignes_frais.adresse-mail where valide=0";
-        $result = mysql_query($query);
-        $renvoi2 = "";
-        	 while ($data2 = mysql_fetch_array($result)) 
-        { 
-            $renvoi2 .= '<option value="">'.$data2['motif'].'</option>'; 
+    /**
+     * Description of class_Frais
+     *
+     * @author thibaud
+     */
+    class class_Frais {
+        //put your code here
+
+        public function __construct() 
+           {
+
+           }
+
+        public function GetUtlisateur()
+        {
+                $host = "localhost";
+                    $user = "root";
+                    $bdd = "fredi";
+                    $passwd  = "";
+
+                    mysql_connect($host, $user,$passwd) or die("erreur de connexion au serveur");
+                        mysql_select_db($bdd) or die("erreur de connexion a la base de donnees");
+
+                $query = "select nom,prenom from demandeurs";
+                    //inner join lignes_frais on demandeurs.adresse-mail = lignes_frais.adresse-mail where valide=0";
+                $result = mysql_query($query);
+                $renvoi2 = "";
+                         while ($data2 = mysql_fetch_array($result)) 
+                { 
+                    $renvoi2 .= '<option value="">'.$data2['prenom'].' '.$data2['nom'].'</option>'; 
+                }      
+                return $renvoi2;
         }
-        return $renvoi2;
-}
-}
+        public function getNumeroFicheDeFrais()
+        {
+                    $host = "localhost";
+                    $user = "root";
+                    $bdd = "fredi";
+                    $passwd  = "";
+
+                    mysql_connect($host, $user,$passwd) or die("erreur de connexion au serveur");
+                        mysql_select_db($bdd) or die("erreur de connexion a la base de donnees");
+
+                $query = "select motif from lignes_frais";
+                    //inner join lignes_frais on demandeurs.adresse-mail = lignes_frais.adresse-mail where valide=0";
+                $result = mysql_query($query);
+                $renvoi2 = "";
+                         while ($data2 = mysql_fetch_array($result)) 
+                { 
+                    $renvoi2 .= '<option value="">'.$data2['motif'].'</option>'; 
+                }
+                return $renvoi2;
+        }
+
+        function ajoutFichedeFrais($mail, $motif, $nbkm, $peage, $hebergement,$trajet)
+        {
+            $today = date("j, n, Y");
+            $requete = "
+                INSERT INTO `lignes_frais`
+                (`adresse-mail`, `date`, `motif`, `trajet`, `km`, `cout-peage`, `cout-repas`, `cout-hebergement`, `km-validé`, `peage-validé`, `repas-validé`, `hebergement-validé`, `valide`) 
+                VALUES ('".$mail."', '".$today."', '".$motif."', '".$nbkm."', '".$trajet."', '".$nbkm."', '".$peage."', '".$hebergement."','0','0','0','0','0')
+                ";
+            $test = mysql_query($requete);
+            if ($test == true) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+    }
+
+
 ?>
---
+++ traitementFiche.php
@@ -0,0 +1,50 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+         include 'navigation.php'; 
+         
+        
+        ?>
+        
+        <?php 
+        
+            $mail = $_POST['mail'];
+            $motif = $_POST['Fiche_Motif'];
+            $nbkm = $_POST['Fiche_Km'];
+            $peage = $_POST['Fiche_Cout-peage'];
+            $hebergement = $_POST['Fiche_Cout-hebergement'];
+            $trajet = $_POST['Fiche_trajet'];
+        
+            require_once ("class_connexionBDD.php");
+            require_once ("class_Frais.php");
+            
+            $coBDD = new connexion();
+            $objFrais = new class_Frais();
+            
+            $coBDD->connexionBDD();
+            $test = $objFrais->ajoutFichedeFrais($mail, $motif, $nbkm, $peage, $hebergement,$trajet);
+            
+            if ($test == true) 
+            {
+                echo'<div id="Fiche_valid">Votre fiche de frais à bien été prise en compte.<br>Elle sera soumise à la validation de notre agent comptable, puis vous vous verrez rembourser vos frais de déplacement</div>';
+            }
+            else
+            {
+                echo'<div id="Fiche_valid">Un ou plusieurs problèmes ont été rencontrés durant pendant le stockage de votre fiche de frais.<br>Excusez nous pour la géne occasionnée</div>';
+            }
+            
+            $coBDD->deconnexionBDD();
+            
+        ?>
+    </body>
+</html>

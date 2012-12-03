1d293f0ce64f4717cd8c23ffc2c996e6127930b5
mikael.thibaut <mikael.thibaut@BE-S306-P2085.BtsSio-Carriat.com>
2012-12-03 16:53:27 +0100

    CERFA Mick

--- cerfa.php
+++ cerfa.php
@@ -6,11 +6,35 @@ class PDF extends FPDF
 // En-tête
 function Header()
 {
-    // Logo
-    $this->Image('logo-cerfa.png',10,6,30);
+    // Logo1
+    $this->Image('logo-cerfa.png',50,6,100);
 	$this->Ln(10);
+        
+    // Logo2
+    $this->Image('logo-cerfa2.png',50,16,100);
+	$this->Ln(10);
+        
+    // Logo3
+    $this->Image('logo-cerfa3.png',50,30,100);
+	$this->Ln(10);
+    
+    // Logo4
+    $this->Image('logo-cerfa4.png',50,80,100);
+	$this->Ln(10);
+        
+        
+        
+        
 }
+
+
+
+
+
 }
+
+
+
 $pdf = new PDF();	
 $pdf->Output();
 

d1cfb0e110329bd7b19dada06f3ea6e5c810683a
mikael.thibaut <mikael.thibaut@BE-S306-P2085.BtsSio-Carriat.com>
2012-12-03 16:35:24 +0100

    CERFA Mick

--- navigation.php
+++ navigation.php
@@ -16,7 +16,8 @@ else
             <div id="home_link"><a href="home.php">HOME</a></div>
             <div id="profil_link"><a href="GererProfil.php">PROFIL</a></div>
             <div id="Outils_link"><a href="ficheFrais.php?action=Choix">Administration des fiches</a></div>
-            <div id="PDF_link"><a href="pdff.php">Edition PDF et CERFA</a></div>
+            <div id="PDF_link"><a href="pdff.php">Edition PDF</a></div>
+            <div id="PDF_link"><a href="cerfa.php">Edition CERFA</a></div>
             <div id="deconnexion_link"><a href="deconnexion.php">DECONNEXION</a></div>        
         </div>'; 
 }
